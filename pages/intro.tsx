a373dbaa819b00e9398a26f37e292ec1f4bf7866
Charlotte Cameron <charlotte.cameron@adenin.com>
2022-12-22 21:20:11 +0000

    Add intro page

---
+++ pages/intro.tsx
@@ -0,0 +1,89 @@
+import Link from 'next/link';
+import Layout from '../components/Layout';
+import PageHeader from '../components/PageHeader';
+import PageSection from '../components/PageSection';
+
+export default function Intro() {
+  return (
+    <>
+      <Layout showChapters>
+        <PageHeader id="intro-header" title="Introduction">
+          <p>
+            Having a disability can change how a person perceives, navigates and
+            interacts with the Web. By prioritizing Web accessibility from the
+            start of the design process throughout the end of development, we
+            can ensure that people have the same <em>access to</em> and{' '}
+            <em>experience on</em> the Web, irrespective of if they have a
+            disability or not.
+          </p>
+        </PageHeader>
+        <PageSection id="what-is-web-accessibility" title="What is Web Accessibility?">
+          <p>
+            Text text text
+          </p>
+          <p>
+            Accessible design and development practices help a whole spectrum of
+            people with different related abilities. This includes:
+          </p>
+          <ul>
+            <li>
+              Permanent disabilities: long-term impairments which pose barriers
+              to the full and effective participation in society on an equal
+              basis with others, e.g. a person with diabetes who has partial
+              vision loss.
+            </li>
+            <li>
+              Temporary limitations: limitations which are brought about by
+              injury or illness, e.g. a person using the Web with one hand due
+              to a broken arm.
+            </li>
+            <li>
+              Situational limitations: limitations which are caused by a
+              person&apos;s environment, e.g. a person using the Web on a mobile
+              device with a small screen in direct bright light.
+            </li>
+          </ul>
+        </PageSection>
+        <PageSection id="about-this-site" title="About this Site">
+          <p>
+            This site aims to provide a hands-on experience of the impact poor
+            design and development patterns can have on Web accessibility. These
+            patterns create barriers which can exclude people, or whole groups
+            of society, from using the Web. It also highlights how inclusive
+            design and development fosters belonging and makes the web more
+            accessible for everyone.
+          </p>
+          <p>
+            The simulations on this site are not medically or scientifically
+            accurate. Impairment simulations cannot convey what it is really
+            like to live with capability loss on an everyday basis. However, it
+            can be helpful to understand and empathize with the challenges faced
+            by people with disabilities when navigating the Web.
+          </p>
+          <p>
+            There are numerous excellent resources online which share more information about
+            building accessible Web experiences. This includes technical guidance and best
+            practices, and is typically aimed at designers and developers. The{' '}
+            <Link
+              href="https://www.w3.org/WAI/standards-guidelines/wcag/"
+              target="_blank"
+              rel="noopener"
+            >
+              Web Content Accessibility Guidelines (WCAG)
+            </Link>
+            , published by the{' '}
+            <Link href="https://www.w3.org/WAI/" target="_blank" rel="noopener">
+              Web Accessibility Initiative (WAI)
+            </Link>
+            , are a gold-standard set of recommendations for creating accessible
+            content on the Web.
+          </p>
+          <p>
+            P.S. A number of the simulations shown on this site are available in
+            the navigation under the &lsquo;Settings&rsquo; dropdown.
+          </p>
+        </PageSection>
+      </Layout>
+    </>
+  );
+}
