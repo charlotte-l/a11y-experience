23121fd4676e5fbaf75ca126664baf3efc894186
Charlotte Cameron <charlotte.cameron@adenin.com>
2022-12-26 19:54:46 +0000

    Update color blind copy and demo

--- components/demo/visual/color-blind/index.tsx
+++ components/demo/visual/color-blind/index.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import styled from 'styled-components';
 import TextWrapper from '../../TextWrapper';
 import Playground from '../../../Playground';
 import Overlay from '../Overlay';
@@ -7,6 +8,19 @@ import SelectControl from '../../../controls/SelectControl';
 import Alert from '../../Alert';
 import TubeStations from './TubeStations';
 import stations from './tubeStationData';
+import Container from '../../../Container';
+import Form from './Form';
+
+const FormHeading = styled.span`
+  display: block;
+  margin-top: 3rem;
+  margin-bottom: 0.5rem;
+  font-size: 1.17em;
+  font-weight: bold;
+  line-height: 1.2;
+  font-family: Cambria, 'Hoefler Text', Utopia, 'Liberation Serif',
+    'Nimbus Roman No9 L Regular', Times, 'Times New Roman', serif;
+`;
 
 const ColorBlind = () => (
   <Playground
@@ -32,47 +46,61 @@ const ColorBlind = () => (
       />,
     ]}
   >
-    <Overlay type="color">
-      <TextWrapper>
-        <p>
-          Greater London is served by{' '}
-          <Link
-            href="https://tfl.gov.uk/tube-dlr-overground/status/"
-            target="_blank"
-            rel="noopener nofollow"
-            css={`
-              text-decoration: none;
-              &:hover {
-                background-color: transparent;
-              }
-            `}
-          >
-            11 Tube lines
-          </Link>
-          , along with the Docklands Light Railway (DLR), the London
-          Overground,the Elizabeth line and National Rail services.
-        </p>
-        <Alert
-          $level="warning"
-          message="There are delays on the Jubilee line due to bad weather conditions."
+    <Container css={'padding: 1rem 1rem 3rem'}>
+      <Overlay type="color">
+        <TextWrapper>
+          <p>
+            Greater London is served by{' '}
+            <Link
+              href="https://tfl.gov.uk/tube-dlr-overground/status/"
+              target="_blank"
+              rel="noopener nofollow"
+              css={`
+                text-decoration: none;
+                &:hover {
+                  background-color: transparent;
+                }
+              `}
+            >
+              11 Tube lines
+            </Link>
+            , along with the Docklands Light Railway (DLR), the London
+            Overground, the Elizabeth line and{' '}
+            <Link
+              href="https://tfl.gov.uk/tube-dlr-overground/status/"
+              target="_blank"
+              rel="noopener nofollow"
+            >
+              National Rail services
+            </Link>
+            .
+          </p>
+          <Alert
+            $level="error"
+            message="There are delays on the Jubilee line due to bad weather conditions."
+          />
+          <TubeStations
+            stations={stations}
+            styles={'text-align: center; margin: 1.5rem 0;'}
+          />
+          <p>Below, you can see a map of tube stations in central London:</p>
+        </TextWrapper>
+        <Image
+          alt="A color map of the London Underground showing a central section of stations"
+          src="/london-underground.png"
+          width="1367"
+          height="879"
+          style={{ height: 'auto', marginBlockEnd: '1.5em' }}
         />
-        <br/>
-        <TubeStations stations={stations} />
-        <br/>
-        <p>Below, you can see a map of tube stations in central London:</p>
-      </TextWrapper>
-      <Image
-        alt="A color map of the London Underground showing a central section of stations"
-        src="/london-underground.png"
-        width="1367"
-        height="879"
-        style={{ height: 'auto', marginBlockEnd: '1.5em' }}
-      />
-      <Alert
-        $level="success"
-        message="Step free access is now available on the Elizabeth line."
-      />
-    </Overlay>
+        <Alert
+          $level="success"
+          message="Step free access is now available on the Elizabeth line."
+          noIcon={true}
+        />
+        <FormHeading>Sign up for status updates</FormHeading>
+        <Form />
+      </Overlay>
+    </Container>
   </Playground>
 );
 
--- pages/visual-disabilities.tsx
+++ pages/visual-disabilities.tsx
@@ -62,15 +62,58 @@ export default function VisualDisability() {
           title="Color Blindness"
         >
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed purus
-            augue, aliquet nec dolor in, aliquam rhoncus augue. Etiam
-            consectetur, ante eu gravida faucibus, leo leo gravida nibh, id
-            convallis tellus felis sit amet lorem. Quisque feugiat nec mauris
-            quis euismod. In volutpat hendrerit augue, vel cursus justo faucibus
-            ac. Nunc dolor leo, malesuada in accumsan commodo, facilisis ut
-            justo.
+            People with color blindness are less able to see certain colors, or differences
+            between colors. This usually happens between greens and reds, and occasionally blues.
+            Color blindness can be genetic (inherited from a parent) or acquired due to illness,
+            injury, ageing or some medications.
+          </p>
+          <p>
+            Some people with achromatopsia (total color blindness) also find
+            it more difficult to see in brightly-lit places, and have a reduced ability to
+            discern the shapes and details of the things they see.
+          </p>
+          <p>
+            There are a number of types of color blindness. They are caused by the type of cells
+            in the eye which are affected. These include:
+          </p>
+          <ul>
+            <li>Protanopia: unable to perceive red light</li>
+            <li>Protanomaly: reduced sensitivity to red light</li>
+            <li>Deuteranopia: unable to perceive green light</li>
+            <li>Deuteranomaly: reduced sensitivity to green light</li>
+            <li>Tritanopia: unable to perceive blue light</li>
+            <li>Tritanomaly: reduced sensitivity to blue light</li>
+            <li>Achromatopsia: unable to perceive red, green or blue light</li>
+          </ul>
+          <p>
+            Being color blind doesn&apos;t mean only being unable to perceive a specific color.
+            Any color which <em>includes</em> that color could be confused for another color. As
+            an example, purple, which is comprised of red and blue light, would be perceived
+            similarly to a person with protanopia-type color blindness.
+          </p>
+          <p>
+            Red-green color blindness affects up to 1 in 12 males (8%) and 1 in 200 females (0.5%).
+            Worldwide, there are estimated to be about 300 million people with color blindness.
           </p>
 
+          <h3>Designing for color blindness</h3>
+
+          <p>
+            Make sure that colors are not the only method of conveying important information:
+          </p>
+          <ul>
+            <li>Ensure links are distinguishable from text in ways other than color</li>
+            <li>Use contrast (lightness) to convey meaning as well as hue</li>
+            <li>Use descriptive text labels where color is the subject</li>
+            <li>Use iconography and descriptive labels to convey intent</li>
+            <li>Use texture and patterns as well as color to convey information</li>
+          </ul>
+          <p>
+            In the demo below, you can see how different types of color blindness can affect a
+            person&apos;s access to information on the Web. Notice how certain design patterns
+            make information more difficult to convey, whilst others enable people with
+            color blindness to access information and interact with the page content.
+          </p>
           <ColorBlind />
         </PageSection>
 
