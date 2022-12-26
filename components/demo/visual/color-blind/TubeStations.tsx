945d8bfdd67fe0762b090c518339eae8fc3a596f
Charlotte Cameron <charlotte.cameron@adenin.com>
2022-12-26 19:53:46 +0000

    Add background textures to some tube stations

--- components/demo/visual/color-blind/TubeStations.tsx
+++ components/demo/visual/color-blind/TubeStations.tsx
@@ -1,27 +1,29 @@
+import { CSSProp } from 'styled-components';
 import Grid from '../../Grid';
 import GridItem from '../../GridItem';
 
-type StationsProps = {
+type TubeStationProps = {
   stations: Array<{
     label: string,
-    bgColor: string,
+    background: string,
+    bgSize?: string,
     textColor: string,
-  }>
+  }>,
+  styles: CSSProp<any>,
 };
 
-const TubeStations = ({ stations }: StationsProps) => (
+const TubeStations = ({ stations, styles }: TubeStationProps) => (
   <Grid
     columns="repeat(5, 1fr)"
     rows="repeat(3, 1fr)"
-    styles={`
-        text-align: center;
-    `}
+    styles={styles}
   >
     {stations.map((station) => (
       <GridItem
         key={station.label}
         styles={`
-          background: ${station.bgColor};
+          background: ${station.background};
+          background-size: ${station.bgSize || 'cover'};
           color: ${station.textColor};
         `}
       >
