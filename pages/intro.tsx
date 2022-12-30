b3eecb9a7df4a38eed7c439fd5f548a3a0c33bc7
Charlotte Cameron <charlotte.cameron@adenin.com>
2022-12-30 19:19:05 +0000

    Add Why section to intro page

--- pages/intro.tsx
+++ pages/intro.tsx
@@ -17,11 +17,15 @@ export default function Intro() {
             disability or other limitations, or not.
           </p>
         </PageHeader>
-        <PageSection id="what-is-web-accessibility" title="What is Web Accessibility?">
+        <PageSection
+          id="what-is-web-accessibility"
+          title="What is Web Accessibility?"
+        >
           <p>
-            Web accessibility is ensuring there are no barriers between people and their access
-            to and usage of the web. This includes physical disabilities, situational limitations,
-            and things like internet speed or data allowance. Designing and developing for Web
+            Web accessibility is ensuring there are no barriers between people
+            and their access to and usage of the web. This includes physical
+            disabilities, situational limitations, and things like internet
+            speed or data allowance. Designing and developing for Web
             accessibility means more people have equal access to the Web.
           </p>
           <p>
@@ -36,9 +40,9 @@ export default function Intro() {
               vision loss.
             </li>
             <li>
-              Temporary limitations: limitations which are caused by
-              injury or illness, e.g. a person using the Web with one hand due
-              to a broken arm.
+              Temporary limitations: limitations which are caused by injury or
+              illness, e.g. a person using the Web with one hand due to a broken
+              arm.
             </li>
             <li>
               Situational limitations: limitations which are caused by a
@@ -46,11 +50,41 @@ export default function Intro() {
               device with a small screen in direct bright light.
             </li>
             <li>
-              Socio-economic limitations: limitations which are caused by having reduced economic
-              access to resources and social position in relation to others than others in the
-              same society.
+              Socio-economic limitations: limitations which are caused by having
+              reduced economic access to resources and social position in
+              relation to others than others in the same society.
             </li>
           </ul>
+        </PageSection>
+        <PageSection id="why" title="Why Web Accessibility matters">
+          <p>
+            Web Accessibility matters for a number of reasons. There are
+            practical reasons such as legal requirements &ndash; the Americans
+            with Disabilities Act (ADA) requires that any person or organization
+            must make their website &lsquo;readily accessible and usable by
+            individuals with disabilities if it is for use by the public&rsquo;.
+            There are also business opportunities &ndash; in the US alone, the
+            annual discretionary spending of people with disabilities is over
+            $200 billion.
+          </p>
+          <p>
+            Moreover, Web Accessibility drives innovation and promotes social
+            inclusion. Diversity facilitates belonging, brings new perspectives
+            to light and enhances creativity. This benefits business, society,
+            and everyone in it. Accessibility is for everyone.
+          </p>
+          <p>
+            Kat Holmes&apos;{' '}
+            <Link
+              href="https://mitpress.mit.edu/9780262539487/mismatch/"
+              target="_blank"
+              rel="nofollow noopener"
+            >
+              &lsquo;Mismatch: How Inclusion Shapes Design&rsquo;
+            </Link>
+            {' '} is a compelling read which goes into more detail about the power
+            of inclusive design and how to approach it.
+          </p>
         </PageSection>
         <PageSection id="about-this-site" title="About this Site">
           <p>
@@ -69,9 +103,10 @@ export default function Intro() {
             by people with disabilities when navigating the Web.
           </p>
           <p>
-            There are numerous excellent resources online which share more information about
-            building accessible Web experiences. This includes technical guidance and best
-            practices, and is typically aimed at designers and developers. The{' '}
+            There are numerous excellent resources online which share more
+            information about building accessible Web experiences. This includes
+            technical guidance and best practices, and is aimed at designers and
+            developers. The{' '}
             <Link
               href="https://www.w3.org/WAI/standards-guidelines/wcag/"
               target="_blank"
